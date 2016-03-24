8456b8f2af4461b3eaa5d433e947c909d30af883
Eunice13 <khcy4ewi@nottingham.edu.my>
2016-03-24 16:22:17 +0800

    Added 'Add Guard' form

--- Parent/settings.php
+++ Parent/settings.php
@@ -55,13 +55,148 @@
 		</div>
 	</nav>
 
+	<div class="row">
+		<div class="col-lg-8" style="margin-left:40px">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Guard Information
+				</div>
+		<div class="panel-body">
+			<div role="tabpanel">
+				<ul class= "nav nav-tabs">
+					<li role="presentation" class="active"><a href="#add" data-toggle="tab">Add Guard</a></li>
+					<li role="presentation"><a href="#edit" data-toggle="tab">Remove Guard</a></li>
+				</ul>
+
+				<div class="tab-content">
+					<div class="tab-pane fade in active" id="add">
+						<form role="form" action="add.php">
+							<div class="row" style="margin-top: 20px;">
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        <label>Name</label>
+                                        <input type="text" class="form-control" placeholder="Name" name="name" id="name">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>IC Number</label>
+                                        <input type="text" class="form-control" placeholder="IC Number" name="IC_No" id="IC_No">
+                                    </div>
+                                </div>
+                            </div>
+
+                                    <div class="row">
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label>Date of Birth</label>
+                                                <input type="text" class="form-control" placeholder="Date of Birth" >
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <label>Gender</label>
+                                                <input type="text" class="form-control" placeholder="Gender" >
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Address</label>
+                                                <input type="text" class="form-control" placeholder="Address">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label>Race</label>
+                                                <input type="text" class="form-control" placeholder="Race" >
+                                            </div>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label>Religion</label>
+                                                <input type="text" class="form-control" placeholder="Religion" >
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label>Contact Number</label>
+                                                <input type="text" class="form-control" placeholder="Contact Number" >
+                                            </div>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label>Registration Number</label>
+                                                <input type="date" class="form-control" placeholder="Registration Number">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Category</label>
+                                                <input type="text" class="form-control" placeholder="Category" >
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Date</label>
+                                                <input type="date" class="form-control" placeholder="Date">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Check In Time</label>
+                                                <input type="time" class="form-control" placeholder="Check In Time">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Check Out Time</label>
+                                                <input type="time" class="form-control" placeholder="Check Out Time">
+                                            </div>
+                                        </div>
+        
+                                    </div>
+                                     <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Remarks</label>
+                                                <input type="time" class="form-control" placeholder="Remarks">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-info btn-fill pull-right">Add</button>
+                                    <div class="clearfix"></div>
+						</form>
+					</div>
+
+					<div class="tab-pane fade" id="remove">
+                        
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+
 	<?php 
 		if($login_session=="admin@vms.com"){
 		echo "admin stuff goes here";
 	}else{
 		echo '<div class="alert alert-warning" role="alert">
   			<a href="" class="alert-link">something</a></div>';
-	}
-	?>
+	}?>
 </body>
 </html>
