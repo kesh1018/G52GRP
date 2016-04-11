54036c76af7f97036da0e89992f21e5201e1c03f
Yushafizal Yusri <yushafizalyusri@gmail.com>
2016-04-12 00:08:10 +0800

    added fetch data class

---
+++ child/android-prototype/MyApplication5/app/src/main/java/com/example/yusha/myapplication/Config.java
@@ -0,0 +1,25 @@
+package com.example.yusha.myapplication;
+
+/**
+ * Created by yusha on 11/04/2016.
+ */
+public class Config {
+    public static final String DATA_URL = "http://10.0.2.2/login-app/fetchData.php";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_IC_NO = "IC_No";
+    public static final String KEY_DOB = "dob";
+    public static final String KEY_GENDER = "gender";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_RACE = "race";
+    public static final String KEY_RELIGION = "religion";
+    public static final String KEY_CONTACT_NO = "contact_no";
+    public static final String KEY_REGISTRATION_NO = "registration_no";
+    public static final String KEY_CATEGORY = "category";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_CHECK_IN = "check_in";
+    public static final String KEY_CHECK_OUT = "check_out";
+    public static final String KEY_REMARKS = "remarks";
+    public static final String KEY_BLACKLIST = "blacklist";
+    public static final String JSON_ARRAY = "result";
+
+}
