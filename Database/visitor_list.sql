28a06544a455566d9f87e3b50fee7b0a2801fec1
Eunice13 <khcy4ewi@nottingham.edu.my>
2016-04-15 01:09:44 +0800

    Added entrypass_num

--- Database/visitor_list.sql
+++ Database/visitor_list.sql
@@ -17,6 +17,7 @@ vehicle_type	varchar(25) NOT NULL,
 registration_no varchar(255) NOT NULL,
 date		date NOT NULL,
 category	varchar(15) NOT NULL,
+entrypass_num	varchar(25) NOT NULL, 
 check_in	time(6) NOT NULL,
 check_out	time(6) NOT NULL,
 remarks   	varchar (1000) NOT NULL,
@@ -29,10 +30,10 @@ PRIMARY KEY(ID)
 # *************************
 # *** DATA FOR SCENARIO ***
 # *************************
-INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, check_in, check_out, remarks, blacklist) VALUES ('Eunice', '950113130000', '1995-01-13', 'Female', 'Pangkor Hall', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Emergency', '12:00', '14:00', 'Visit relatives', '-');
-INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, check_in, check_out, remarks, blacklist) VALUES ('Daniel', '123', '1995-01-13', 'Male', 'Shah Alam', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Emergency', '12:00', '14:00', 'Visit relatives', '-');
-INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, check_in, check_out, remarks, blacklist) VALUES ('Kesh', '456', '1995-01-13', 'Male', 'Petaling Jaya', 'India', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Visitor', '10:00', '14:00', 'Visit relatives', '-');
-INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, check_in, check_out, remarks, blacklist) VALUES ('Sunny', '73829', '1995-01-13', 'Female', 'Selangor', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-06', 'Delivery', '21:00', '21:30', 'Visit relatives', '-');
+INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, entrypass_num, check_in, check_out, remarks, blacklist) VALUES ('Eunice', '950113130000', '1995-01-13', 'Female', 'Pangkor Hall', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Emergency', 'ZZZ987', '12:00', '14:00', 'Visit relatives', '-');
+INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, entrypass_num, check_in, check_out, remarks, blacklist) VALUES ('Daniel', '123', '1995-01-13', 'Male', 'Shah Alam', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Emergency', 'ZZZ987', '12:00', '14:00', 'Visit relatives', '-');
+INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, entrypass_num, check_in, check_out, remarks, blacklist) VALUES ('Kesh', '456', '1995-01-13', 'Male', 'Petaling Jaya', 'India', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-03', 'Visitor', 'ZZZ987', '10:00', '14:00', 'Visit relatives', '-');
+INSERT INTO visitor_list (name, IC_No, dob, gender, address, race, religion, contact_no, vehicle_type, registration_no, date, category, entrypass_num, check_in, check_out, remarks, blacklist) VALUES ('Sunny', '73829', '1995-01-13', 'Female', 'Selangor', 'Cina', 'Kristian', '0109744556', 'Car', 'AAA1234', '2016-02-06', 'Delivery', 'ZZZ987', '21:00', '21:30', 'Visit relatives', '-');
 
 commit;
 
