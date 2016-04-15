35716503cac3150a4d7592552fb0d777e3180a2a
Yushafizal Yusri <yushafizalyusri@gmail.com>
2016-04-15 22:17:55 +0800

    deleted:    Android-Application/android-prototype/login-app/BACK.php 	deleted:    Android-Application/android-prototype/login-app/insertDataBak.php 	deleted:    Android-Application/android-prototype/login-app/password.php

--- Android-Application/android-prototype/login-app/password.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-	define('HOST','localhost');
-	define('USER','root');
-	define('PASS','');
-	define('DB','visitor_list');
-	
-	$con = mysqli_connect(HOST,USER,PASS,DB) or die('unable to connect to db');
